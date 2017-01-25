0c165c462155bbf2bba22c48c9cc29a829973abf
Mckinley <mckinley.s.hill@gmail.com>
2017-01-25 18:45:25 +0000

    T

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-var locale = require('locale')
-var os = require('os')
+
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-    var ip = os.networkInterfaces();
-    var soft = os.platform();
-    var lang = req.acceptsLanguages;
-    res.send(soft);
+    var ip= req.ips;
+    res.send(ip);
 });
 
 app.listen(PORT, function(){
