d368127d1e335a1acacb8a10f420055adaadb752
Mckinley <mckinley.s.hill@gmail.com>
2017-01-25 18:08:24 +0000

    added ip detection, OS detection, browser language detection

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+var locale = require('locale')
 var os = require('os')
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-    var info = os.cpus();
-    res.send(info[0].model);
+    var ip = os.networkInterfaces();
+    var soft = os.platform();
+    var lang = req.acceptsLanguage;
+    res.send(ip + soft + lang);
 });
 
 app.listen(PORT, function(){
