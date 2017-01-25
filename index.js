eadc300e75335a441bb51b663e934c1304c33ae1
Mckinley <mckinley.s.hill@gmail.com>
2017-01-25 17:58:21 +0000

    T

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var PORT = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
     var info = os.cpus();
-    res.send(info.model);
+    res.send(info[0].model);
 });
 
 app.listen(PORT, function(){
