c5cbad2deeda7afb5abbb1f32eaf1c31ecf9b3b7
Mckinley <mckinley.s.hill@gmail.com>
2017-01-26 02:58:24 +0000

    added language and software detection

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-    app.enable('trust proxy');
-    var ip= req.ip;
-    res.send(ip);
+    res.send(getInfo(req));
 });
 
 app.listen(PORT, function(){
     console.log('Listening on port: ' + PORT)
 });
+
+function getInfo(req){
+    app.enable('trust proxy');
+    var ip= req.ip;
+    var lang = req.get('accept-language');
+    var soft = req.get('user-agent')
+    return(ip+"     "+soft+"     "+lang)
+    
+}
